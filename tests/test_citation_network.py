939569375406fc6c02b30fb21a95bd02a217e2e6
Fabio Dias <fabio.dias@gmail.com>
2019-04-05 20:11:18 -0400

    gotta double check same_article author

--- tests/test_citation_network.py
+++ tests/test_citation_network.py
@@ -1,8 +1,14 @@
 import pytest
 from src.refcliq.citations import CitationNetwork
+from src.refcliq.preprocess import import_bibs
+from os.path import dirname, realpath, join
 
 class TestCN(object):
     def test_build(self):
-        pass
+        articles=import_bibs([join(dirname(realpath(__file__)),'basic.bib'),])
+        cn=CitationNetwork()
+        cn.build(articles)
+        assert(len(cn._G)==3)
+        assert(len(cn._G.edges())==6)
     def test_add(self):
         pass
